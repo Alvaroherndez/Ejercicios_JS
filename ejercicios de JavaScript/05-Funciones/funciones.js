3dfcaa665bf8d887b59895391dddef9f44962b35
Alvaro Hernandez <156653343+Alvaroherndez@users.noreply.github.com>
2024-03-10 21:21:39 +0100

    Add files via upload

---
+++ ejercicios de JavaScript/05-Funciones/funciones.js
@@ -0,0 +1,17 @@
+// Función para sumar dos números
+function suma(a, b) {
+    return a + b;
+}
+
+// Función para restar dos números
+function resta(a, b) {
+    return a - b;
+}
+
+// Realizar la suma
+let resultadoSuma = suma(23, 46);
+console.log("Resultado de la suma:", resultadoSuma);
+
+// Realizar la resta
+let resultadoResta = resta(64, 23);
+console.log("Resultado de la resta:", resultadoResta);
