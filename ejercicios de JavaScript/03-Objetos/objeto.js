39162820ae5ab552a804d0f56c8b239652ec9be4
Alvaro Hernandez <156653343+Alvaroherndez@users.noreply.github.com>
2024-03-10 21:12:24 +0100

    Add files via upload

---
+++ ejercicios de JavaScript/03-Objetos/objeto.js
@@ -0,0 +1,18 @@
+let animal = {
+    Nombre: "León",
+    Tipo: "Mamífero",
+    Hábitat: "Savanas de África",
+    Edad: 8,
+    Color: "Amarillo"
+};
+
+console.log("Valores del objeto animal:");
+console.log(animal);
+
+// Cambiando los valores de Edad y Color
+animal.Edad = 10;
+animal.Color = "Marrón";
+
+console.log("Nuevos valores de Edad y Color:");
+console.log("Edad:", animal.Edad);
+console.log("Color:", animal.Color);
