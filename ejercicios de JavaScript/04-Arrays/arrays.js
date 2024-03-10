a5059c5047e157285496eeb7b811beb204a5e0e7
Alvaro Hernandez <156653343+Alvaroherndez@users.noreply.github.com>
2024-03-10 21:15:04 +0100

    Add files via upload

---
+++ ejercicios de JavaScript/04-Arrays/arrays.js
@@ -0,0 +1,15 @@
+let animalesTropicales = ["Tucán", "Pez payaso", "Serpiente boa", "Mono araña", "Tigre de Bengala", "Loro", "Jaguar", "Papagayo", "Pez ángel", "Cangrejo ermitaño"];
+
+console.log("Array de animales tropicales:");
+console.log(animalesTropicales);
+
+console.log("Elementos en los índices 2, 4 y 11:");
+console.log(animalesTropicales[2]);
+console.log(animalesTropicales[4]);
+console.log(animalesTropicales[11]);
+
+console.log("Tipo de array:");
+console.log(typeof animalesTropicales);
+
+console.log("Tamaño del array:");
+console.log(animalesTropicales.length);
