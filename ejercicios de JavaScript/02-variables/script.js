c2648feff41c619917fa16f0eeb6947f1faf7a08
Alvaro Hernandez <156653343+Alvaroherndez@users.noreply.github.com>
2024-03-10 21:05:50 +0100

    Add files via upload

---
+++ ejercicios de JavaScript/02-variables/script.js
@@ -0,0 +1,7 @@
+let nombre = "Tu Nombre";
+let apellidos = "Tus Apellidos";
+let animal = "Tu Animal Preferido";
+
+console.log("Nombre:", nombre);
+console.log("Apellidos:", apellidos);
+console.log("Animal Preferido:", animal);
