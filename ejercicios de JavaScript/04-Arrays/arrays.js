3746b8131a443b2a611e05649ae4a9884c7cd239
Alvaro Hernandez <156653343+Alvaroherndez@users.noreply.github.com>
2024-03-10 21:18:22 +0100

    Add files via upload

--- ejercicios de JavaScript/04-Arrays/arrays.js
+++ ejercicios de JavaScript/04-Arrays/arrays.js
@@ -13,3 +13,32 @@ console.log(typeof animalesTropicales);
 
 console.log("Tamaño del array:");
 console.log(animalesTropicales.length);
+
+
+
+let animales = ["León", "Tigre", "Elefante", "Jirafa", "Cebra", "Hipopótamo", "Rinoceronte", "Leopardo", "Gorila", "Canguro"];
+
+console.log("Valores del array:");
+console.log(animales);
+
+console.log("Elementos en los índices 0, 4, 6 y 11:");
+console.log(animales[0]);
+console.log(animales[4]);
+console.log(animales[6]);
+console.log(animales[11]);
+
+// Cambiando el elemento 5 del array
+animales[4] = "Pipito feliz";
+console.log("Array después de cambiar el elemento 5:");
+console.log(animales);
+
+// Añadiendo un nuevo elemento al array
+animales.push("Pipito triste");
+console.log("Array después de añadir 'Pipito triste':");
+console.log(animales);
+
+console.log("Tipo del array:");
+console.log(typeof animales);
+
+console.log("Tamaño del array:");
+console.log(animales.length);
